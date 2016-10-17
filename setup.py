20f8a379629fae4e9db32913efaf65b91cfdbb9c
Josh Snyder <josh@code406.com>
2016-10-16 23:24:02 -0700

    Rename (the Python component) to keypipe

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, Extension
 from setuptools import find_packages
 
 setup(
-    name='aepipe',
+    name='keypipe',
     version='0.0.1',
     author='Josh Snyder',
     author_email='josh@code406.com',
-    packages = ['aepipe'],
+    packages = ['keypipe'],
     setup_requires=["cffi>=1.0.0"],
     install_requires=[
       "cffi>=1.0.0",
