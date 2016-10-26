b7d38faa5ea9cb4123ee2a3d9a7e768457c5a423
Josh Snyder <josh@code406.com>
2016-10-25 22:46:07 -0700

    Hilarious, thanks bash

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-test -n "$MAKE" && make
-test -n "$ENVLIST" && tox -e "$ENVLIST"
+test -n "${MAKE:-}" && make
+test -n "${ENVLIST:-}" && tox -e "$ENVLIST"
